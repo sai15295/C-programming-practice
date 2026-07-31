8e972f006e2b6c0d7c9ac23b8ad214471cc53ed0
sai15295 <ashrithasaiavula91@gmail.com>
2026-07-31 20:17:22 +0530

    Add age eligibility checker program

---
+++ Age eligibility checker.C
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+		
+		int age;
+		printf("Enter your age:  ");
+		scanf("%d",&age);
+		
+		if(age>=18)
+		{
+			printf("You are eligible for voting.\n");
+			printf("Select your leader.");
+		}
+		else
+		{
+			printf("You are not eligible for voting.\n");
+			printf("Good luck.");
+		}
+}
